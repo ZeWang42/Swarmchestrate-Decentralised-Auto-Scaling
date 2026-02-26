b99f67133411351bfb1ce548f9e580a755807269
ZeWang42 <243932558@qq.com>
2026-02-26 13:40:19 +0000

    add bypass sudo

--- k3s-installation/1-install-master.sh
+++ k3s-installation/1-install-master.sh
@@ -21,4 +21,7 @@ sleep 1
 sudo cat /var/lib/rancher/k3s/server/node-token > token.txt
 
 echo "Token saved to token.txt"
-
+# Bypass sudo
+#
+sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
