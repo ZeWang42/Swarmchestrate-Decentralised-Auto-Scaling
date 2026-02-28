685875f427436e73955fd5ed9ea5ad1473f5e3c8
ZeWang42 <243932558@qq.com>
2026-02-28 18:12:26 +0000

    clean up

--- artifacts/sh/microservice-monitoring-Every10s.sh
+++
@@ -1,8 +0,0 @@
-rm monitorResults.log
-while true; do
-  echo "[$(date '+%F %T')]" | tee -a monitorResults.log
-  ./microservice-monitoring.sh | tee -a monitorResults.log
-  echo "" | tee -a monitorResults.log
-  sleep 4
-done
-
--- artifacts/sh/monitorEvery10s.sh
+++
@@ -1,8 +0,0 @@
-rm monitorResults.log
-while true; do
-  echo "[$(date '+%F %T')]" | tee -a monitorResults.log
-  ./monitor.sh | tee -a monitorResults.log
-  echo "" | tee -a monitorResults.log
-  sleep 6
-done
-
