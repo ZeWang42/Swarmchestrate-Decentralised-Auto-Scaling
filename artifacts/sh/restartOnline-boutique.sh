4e19d221b6c400d1aa13560f4e71d08f831d1e92
ZeWang42 <243932558@qq.com>
2026-02-28 17:52:54 +0000

    add frontend gateway

--- artifacts/sh/restartOnline-boutique.sh
+++ artifacts/sh/restartOnline-boutique.sh
@@ -1,4 +1,4 @@
 sudo kubectl delete -f ../yaml/online-boutique.yaml
 sudo kubectl delete -f ../yaml/hpa-online-boutique.yaml
 sudo kubectl apply -f ../yaml/online-boutique.yaml
-#sudo kubectl apply -f hpa-online-boutique.yaml
+sudo kubectl apply -f ../yaml/frontend-gateway.yaml
