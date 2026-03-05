2d4ae8e0b229eccd91c973696f4dc2a31f893cbb
ZeWang42 <33670666+ZeWang42@users.noreply.github.com>
2026-03-05 13:11:46 +0000

    Delete istio-installation/3-install-k8s-GW.sh

--- istio-installation/3-install-k8s-GW.sh
+++
@@ -1,2 +0,0 @@
-kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/experimental-install.yaml
